344dd35f09066ec60b1ee9f1938f96c2f221c39e
M0Hassan <aminmohamedadan@gmail.com>
2022-04-04 12:34:08 +0000

    modified to eslint standards

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const arr = [];
+  const arr = [];
 
-    for (let idx of array) {
-	idx = appendString + idx;
-	arr.push(idx);
-    }
-    return arr;  
+  for (let idx of array) {
+    idx = appendString + idx;
+    arr.push(idx);
+  }
+  return arr;
 }
-
