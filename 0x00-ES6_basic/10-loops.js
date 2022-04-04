ed23fef257ed10c0be4a464f306bcf706afa5507
M0Hassan <aminmohamedadan@gmail.com>
2022-04-04 10:12:38 +0000

    ES6 for...of operator

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const arr = [];
+
+    for (let idx of array) {
+	idx = appendString + idx;
+	arr.push(idx);
+    }
+    return arr;  
+}
+
