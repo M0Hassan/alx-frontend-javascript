8584d7d2d29920d7c178225ed66ccd08829cfa81
M0Hassan <aminmohamedadan@gmail.com>
2022-04-12 11:08:03 +0000

    fixed eslint error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-	throw Error('Cannot process');
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  for (const elements of map) {
+    if (elements[1] === 1) {
+      map.set(elements[0], 100);
     }
-    for (const elements of map) {
-	if (elements[1] === 1) {
-	    map.set(elements[0], 100);
-	}
-    }
-    return map;
+  }
+  return map;
 }
