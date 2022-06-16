a8b61955599bb7ab0d3c4c58b5400bd5abb79a82
M0Hassan <aminmohamedadan@gmail.com>
2022-06-16 09:58:41 +0300

    used the defined prototype

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -3,7 +3,7 @@
 //Prototype: export default class ClassRoom
 //It should accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize
 
-exports.ClassRoom  = class {
+export default class ClassRoom{
     constructor(maxStudentSize) {
         this._maxStudentSize = maxStudentSize;
     }
