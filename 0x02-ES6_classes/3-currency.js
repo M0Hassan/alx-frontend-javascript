8d7ddc39e56610ecc9a9f77383e002e97e136e9c
M0Hassan <aminmohamedadan@gmail.com>
2022-06-16 14:00:07 +0000

    eslint

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Implement a class named Currency:
 
 - Constructor attributes:
@@ -10,28 +10,28 @@ Implement a method named displayFullCurrency that will return the attributes in 
 */
 
 export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(newName) {
-        this._name = this.name;
-    }
+  set name(newName) {
+    this._name = this.name;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(newCode) {
-        this._code = newCode;
-    }
+  set code(newCode) {
+    this._code = newCode;
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
