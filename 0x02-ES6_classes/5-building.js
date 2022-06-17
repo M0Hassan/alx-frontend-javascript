5bb9e7f4c3dc4a543f6a9938a34421b784b8d196
M0Hassan <aminmohamedadan@gmail.com>
2022-06-17 09:37:30 +0000

    eslint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,9 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined)
-       throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   // getter
