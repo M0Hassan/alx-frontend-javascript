7e2c6ba70a75aae9da6204d412365fb272d041bb
M0Hassan <aminmohamedadan@gmail.com>
2022-06-22 08:03:55 +0000

    task_0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,37 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1 : Student = {
+    firstName: 'John',
+    lastName: 'Valenzani',
+    age: 19,
+    location: 'Uruguay',
+}
+
+const student2 : Student = {
+    firstName: 'George',
+    lastName: 'Pacheco',
+    age: 27,
+    location: 'Earth',
+}
+
+const studentList = {student1, student2};
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentList.forEach(obj) => {
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    const location = document.createElement('td'):
+    name.textContent = obj.firstName;
+    location.textContent = obj.location;
+    row.appendChild(name):
+    row.appendChild(location);
+    tbody.appendChild(row);
+    });
+table.appendChild(tbody);
+document.body.appendChild(table);
