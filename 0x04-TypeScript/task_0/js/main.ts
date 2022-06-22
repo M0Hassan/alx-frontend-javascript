174a99c32e0d7eb76fe88bcfebe18d17e0931cc9
M0Hassan <aminmohamedadan@gmail.com>
2022-06-22 18:35:08 +0300

    modified

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -19,19 +19,19 @@ const student2 : Student = {
     location: 'Earth',
 }
 
-const studentList = {student1, student2};
+const studentList = [student1, student2];
 const table = document.createElement('table');
 const tbody = document.createElement('tbody');
 
-studentList.forEach(obj) => {
-    const row = document.createElement('tr');
-    const name = document.createElement('td');
-    const location = document.createElement('td'):
-    name.textContent = obj.firstName;
-    location.textContent = obj.location;
-    row.appendChild(name):
-    row.appendChild(location);
-    tbody.appendChild(row);
-    });
+studentList.forEach((obj) => {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellLocation = document.createElement('td');
+  cellName.textContent = obj.firstName;
+  cellLocation.textContent = obj.location;
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  tbody.appendChild(row);
+});
 table.appendChild(tbody);
 document.body.appendChild(table);
